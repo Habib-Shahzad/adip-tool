49b9e1b445b1b34d8842e6c5815fb6b5b8a9294a
Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>
2022-11-24 09:46:11 +0500

    Faster Clahe (maybe)

--- backend/tool/clahe.py
+++ backend/tool/clahe.py
@@ -1,11 +1,6 @@
 import numpy as np
 import cv2
 import multiprocessing
-   
-  
-
-
-
 
 
 def redistribute(hist, clip_limit):
@@ -27,18 +22,13 @@ def redistribute(hist, clip_limit):
     return (S/len(hist))+bottom
 
 
-def clahe_cdf(Iay):
-    [H, W] = Iay.shape
+def clahe_cdf(region: np.ndarray):
+    [H, W] = region.shape
     NM = W*H
-    EqIay = np.zeros((H, W))
-    histoy = np.zeros(256)
 
-    # Step 1: Calculate the image histogram
-    for j in range(H):
-        for i in range(W):
-            valy = Iay[j][i]
-            if (valy > 0 and valy < 256):
-                histoy[valy] = histoy[valy]+1
+    histoy = np.bincount(region.flatten(), minlength=256)
+    histoy = np.asarray(histoy, dtype=np.float32)
+    len_hist = histoy.size
 
     clip_limit = 3 * np.mean(histoy)
 
@@ -46,26 +36,17 @@ def clahe_cdf(Iay):
     L = clip_limit - P
 
     total_weight = 0
-    for i in range(len(histoy)):
+    
+    for i in range(len_hist):
         if histoy[i] > L:
-            total_weight += (histoy[i]-L)
+            total_weight += (histoy[i] - L)
             histoy[i] = L
 
-    inc = total_weight/len(histoy)
-
-    for i in range(len(histoy)):
-        histoy[i] += inc
-
+    histoy += total_weight/len_hist
+    
     # Step 2: Normalize the image histogram to get PDF
     # Step 3: Integrate PDF to get CDF
-    pdfy = np.zeros(histoy.shape)
-    cdfy = np.zeros(histoy.shape)
-
-    for i in range(len(histoy)):
-        pdfy[i] = histoy[i]/NM
-        cdfy[i] = pdfy[i]
-        if (i > 1):
-            cdfy[i] = cdfy[i]+cdfy[i-1]
+    cdfy = np.cumsum(histoy/NM)
 
     return cdfy
 
@@ -74,19 +55,18 @@ def calc_cdf(region):
     return clahe_cdf(region)
 
 
-def main_CLAHE(input_img, lst):
-
+def main_CLAHE(input_img, coordinates_lst):
+    
     rgbImage = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
     [y, cr, cb] = cv2.split(cv2.cvtColor(rgbImage, cv2.COLOR_RGB2YCR_CB))
     new_y = cv2.copyMakeBorder(y, 8, 8, 8, 8, cv2.BORDER_REFLECT)
-    clahe = np.float32(new_y)
+    clahe = np.float32(new_y)    
+    regions = [new_y[i[0]:i[0]+8, i[1]:i[1]+8] for i in coordinates_lst]
+    
+    with multiprocessing.Pool(5) as p:
+        cdfs = p.map(calc_cdf, regions)
 
-    pool = multiprocessing.Pool()
-    pool = multiprocessing.Pool(processes=30)
-    regions = [new_y[i[0]:i[0]+8, i[1]:i[1]+8] for i in lst]
-    cdfs = pool.map(calc_cdf, regions)
-
-    for index, i in enumerate(lst):
+    for index, i in enumerate(coordinates_lst):
         clahe[i[0], i[1]] = cdfs[index][new_y[i[0], i[1]]]*255
 
     clahe = clahe[8:clahe.shape[0]-8, 8:clahe.shape[1]-8]
